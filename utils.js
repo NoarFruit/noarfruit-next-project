92679a1fa6901fb0834777c449c5ccc36e059f48
Lorei Stojna <>
2025-10-04 15:38:24 +0200

    Refactor: rimosso file e codice non utile dalla configurazione netlify con COPILOT

--- utils.js
+++ utils.js
@@ -1,5 +1,3 @@
-import { uniqueNamesGenerator, adjectives, animals, NumberDictionary } from 'unique-names-generator';
-
 /*
 Get the actual size of a resource downloaded by the browser (e.g. an image) in bytes.
 This is supported in recent versions of all major browsers, with some caveats.
@@ -15,23 +13,6 @@ export function getResourceSize(url) {
     }
 }
 
-// Note: this only works on the server side
-export function getNetlifyContext() {
-    return process.env.CONTEXT;
-}
-
 export function randomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-const uniqueNamesConfig = {
-    dictionaries: [adjectives, animals],
-    separator: '-',
-    length: 2
-};
-
-export function uniqueName() {
-    return uniqueNamesGenerator(uniqueNamesConfig) + "-" + randomInt(100, 999);
-}
-
-export const uploadDisabled = process.env.NEXT_PUBLIC_DISABLE_UPLOADS?.toLowerCase() === "true";
